6fd176b3dbf009d3d689c4006ad2733a9b9f44b6
drame826 <dianguineds04@gmail.com>
2023-03-28 10:56:28 +0000

    c'est bien maintenant

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   enum status: { todo: 0, doing: 1, done: 2 }
   validates :title, presence: true
-  #validates :status, presence: true
+  validates :status, presence: true
   validates :deadline, presence: true
   validate :deadline, :must_start_from_today
 
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,25 +2,39 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe "バリデーション" do
-    it "タイトルが未入力の場合、タスクのバリデーションが無効であること" do
+    it "La validation de la tâche est désactivée si le titre n'est pas saisi" do
       task = Task.new(title: nil, description: "test", status: :todo, deadline: Time.current)
       expect(task).to be_invalid
       expect(task.errors.full_messages).to eq ["Title can't be blank"]
     end
 
-    it "ステータスが未入力の場合、タスクのバリデーションが無効であること" do
+    it "La validation de la tâche est invalide si le statut n'est pas renseigné" do
       task = Task.new(title: "test", description: "test", status: nil, deadline: Time.current)
       expect(task).to be_invalid
       expect(task.errors.full_messages).to eq ["Status can't be blank"]
     end
 
-    it "完了期限が未入力の場合、タスクのバリデーションが無効であること" do
+    it "La validation de la tâche est invalide si le délai de réalisation n'est pas renseigné" do
+      task = Task.new(title: "test", description: "test", status: :todo, deadline: nil)
+      expect(task).to be_invalid
+      expect(task.errors.full_messages).to eq ["Deadline can't be blank"]
+      
+     
+     
     end
 
-    it "完了期限が過去の日付の場合、タスクのバリデーションが無効であること" do
+    it "La validation d'une tâche est invalide si la date d'échéance est dans le passé" do
+      task = Task.new(title: "test", description: "test", status: :todo, deadline: Date.yesterday)
+      expect(task).to be_invalid
+      expect(task.errors.full_messages).to eq ["Deadline must start from today."]
+    
+      
     end
 
-    it "完了期限が今日の日付の場合、タスクのバリデーションが有効であること" do
+    it "La validation de la tâche est valide si la date d'échéance est la date du jour" do
+      task = Task.new(title: "test", description: "test", status: :todo, deadline: Date.today)
+      expect(task).to be_valid
+      expect(task.errors.full_messages).to eq [ ]
     end
   end
 end
